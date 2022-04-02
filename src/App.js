65f175b428e8786a72b649a910b6b77d4d900020
AnnYakusheva <59803594+AnnYakusheva@users.noreply.github.com>
2022-04-02 12:07:23 +0700

    Add Message component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React, { Fragment } from 'react';
 import './App.css';
 import { Button } from './components/Button/Button';
 import { Input } from './components/Input/input';
+import { Message } from './components/Message/message';
 
 const name = "Ivan"
+const message = "Переданный текст"
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <h2>Hello</h2>
       <Input name={name}></Input>
       <Button name={name}/>
+      <Message message={message}></Message>
     </Fragment>
   )
 }
